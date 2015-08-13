b548c94ea8f7bac6bfa2eaf8a896b8359f55b31b
Claudio Schwarz <claudio.schwarz@guavestudios.com>
2015-08-13 18:18:07 +0200

    commit v 0.3

--- includes/components/textblocks.php
+++ includes/components/textblocks.php
@@ -21,12 +21,11 @@
         </section>
       </article>
     </section>
-    <section>
+    <section class="visible-desktop-sm">
       <pre>
         <code class="language-markup">
-
           <?php
-          $code = htmlspecialchars('
+          $imagetext = htmlspecialchars('
             <section class="imagetext">
               <aside class="imagetext__image" role="complementary">
                 <figure>
@@ -49,7 +48,7 @@
               </article>
             </section>
           ');
-          echo $code;
+          echo $imagetext;
           ?>
 
         </code>
@@ -75,6 +74,39 @@
           </p>
         </section>
       </article>
+    </section>
+    <section class="visible-desktop-sm">
+      <pre>
+        <code class="language-markup">
+          <?php
+          $imagetextReverse = htmlspecialchars('
+            <section class="imagetext imagetext--reverse">
+              <aside class="imagetext__image" role="complementary">
+                <figure>
+                  <img src="https://placeholdit.imgix.net/~text?txtsize=33&txt=900%C3%97600&w=900&h=600">
+                </figure>
+              </aside>
+              <article class="imagetext__text">
+                <header>
+                  <h2>
+                    Title
+                  </h2>
+                </header>
+                <section>
+                  <p>
+                    Minions ipsum bananaaaa gelatooo baboiii gelatooo me want bananaaa! Butt poopayee aaaaaah bananaaaa.
+                    Me want bananaaa! aaaaaah hahaha tulaliloo para tú. Hana dul sae ti aamoo! Ti aamoo! Bee do bee do
+                    bee do. Bee do bee do bee do chasy potatoooo pepete. Baboiii poulet tikka masala jiji potatoooo.
+                  </p>
+                </section>
+              </article>
+            </section>
+          ');
+          echo $imagetextReverse;
+          ?>
+
+        </code>
+      </pre>
     </section>
     <section class="textcolumn">
       <article class="textcolumn__text">
@@ -108,10 +140,70 @@
         </section>
       </article>
     </section>
+    <section class="visible-desktop-sm">
+      <pre>
+        <code class="language-markup">
+          <?php
+          $textcolumn = htmlspecialchars('
+            <section class="textcolumn">
+              <article class="textcolumn__text">
+                <header>
+                  <h3>
+                    Title
+                  </h3>
+                </header>
+                <section>
+                  <p>
+                    Minions ipsum bananaaaa gelatooo baboiii gelatooo me want bananaaa! Butt poopayee aaaaaah bananaaaa.
+                    Me want bananaaa! aaaaaah hahaha tulaliloo para tú. Hana dul sae ti aamoo! Ti aamoo! Bee do bee do
+                    bee do. Bee do bee do bee do chasy potatoooo pepete. Baboiii poulet tikka masala jiji potatoooo
+                    hahaha potatoooo tatata bala tu chasy wiiiii.
+                  </p>
+                </section>
+              </article>
+              <article class="textcolumn__text">
+                <header>
+                  <h3>
+                    Title
+                  </h3>
+                </header>
+                <section>
+                  <p>
+                    Minions ipsum bananaaaa gelatooo baboiii gelatooo me want bananaaa! Butt poopayee aaaaaah bananaaaa.
+                    Me want bananaaa! aaaaaah hahaha tulaliloo para tú. Hana dul sae ti aamoo! Ti aamoo! Bee do bee do
+                    bee do. Bee do bee do bee do chasy potatoooo pepete. Baboiii poulet tikka masala jiji potatoooo
+                    hahaha potatoooo tatata bala tu chasy wiiiii.
+                  </p>
+                </section>
+              </article>
+            </section>
+          ');
+          echo $textcolumn;
+          ?>
+
+        </code>
+      </pre>
+    </section>
     <section class="calltoaction">
       <a href="#" class="button calltoaction__button">
         Click me!
       </a>
+    </section>
+    <section class="visible-desktop-sm">
+      <pre>
+        <code class="language-markup">
+          <?php
+          $calltoaction = htmlspecialchars('
+            <section class="calltoaction">
+              <a href="#" class="button calltoaction__button">
+                Click me!
+              </a>
+            </section>
+          ');
+          echo $calltoaction;
+          ?>
+        </code>
+      </pre>
     </section>
   </div>
 </section>
