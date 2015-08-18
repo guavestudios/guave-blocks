5522f82923f1ab9b4f68fbe0bc17ce9e35326924
Claudio Schwarz <claudio.schwarz@guavestudios.com>
2015-08-18 19:12:33 +0200

    Added the grid and some new variables

--- includes/components/grid.php
+++ includes/components/grid.php
@@ -1,2 +1,59 @@
 <section class="container" role="main">
+  <div class="grid">
+    <div class="grid__item one-whole sm-one-half">
+      <p class="demo-block">
+        .one-whole.sm-one-half
+      </p>
+    </div>
+    <div class="grid__item one-whole sm-one-half">
+      <p class="demo-block">
+        .one-whole.sm-one-half
+      </p>
+    </div>
+    <div class="grid__item one-whole sm-one-third">
+      <p class="demo-block">
+        .one-whole.sm-one-third
+      </p>
+    </div>
+    <div class="grid__item one-whole sm-one-third">
+      <p class="demo-block">
+        .one-whole.sm-one-third
+      </p>
+    </div>
+    <div class="grid__item one-whole sm-one-third">
+      <p class="demo-block">
+        .one-whole.sm-one-third
+      </p>
+    </div>
+    <div class="grid__item one-whole sm-one-quarter">
+      <p class="demo-block">
+        .one-whole.sm-one-quarter
+      </p>
+    </div>
+    <div class="grid__item one-whole sm-one-quarter">
+      <p class="demo-block">
+        .one-whole.sm-one-quarter
+      </p>
+    </div>
+    <div class="grid__item one-whole sm-one-quarter">
+      <p class="demo-block">
+        .one-whole.sm-one-quarter
+      </p>
+    </div>
+    <div class="grid__item one-whole sm-one-quarter">
+      <p class="demo-block">
+        .one-whole.sm-one-quarter
+      </p>
+    </div>
+    <div class="grid__item one-whole sm-three-fifths">
+      <p class="demo-block">
+        .one-whole.sm-three-fifths
+      </p>
+    </div>
+    <div class="grid__item one-whole sm-two-fifths">
+      <p class="demo-block">
+        .one-whole.sm-three-fifths
+      </p>
+    </div>
+  </div>
 </section>
